e36c999df8cbcca58d4802cb214ed45b658b8682
delphic <delphic.uk@gmail.com>
2022-02-16 20:05:24 +0000

    Improved auto-scaling and fullscreen logic for 'strange' aspect ratios

--- scripts/client.js
+++ scripts/client.js
@@ -4,6 +4,7 @@ const Fury = require('../fury/src/fury');
 const furyCanvasId = "fury";
 
 let scaleFactor = 1;
+let allElementsHeight = 0;
 let canvasWidth = 640, canvasHeight = 360;
 let canvas;
 
@@ -26,9 +27,17 @@ let dungeonAtlas = {
 window.addEventListener('load', () => {
 	Fury.init({ canvasId: furyCanvasId, glContextAttributes: { antialias: false } });
 
+
 	canvas = document.getElementById(furyCanvasId);
+
+	canvas.height = 0;
+	allElementsHeight = document.getElementsByTagName("html")[0].offsetHeight;
+	// ^^ body.clientHeight does not include top padding, and html element clientHeight is huge for no decernable reason
+
+	// Set canvas to target resolution
 	canvas.width = canvasWidth;
 	canvas.height = canvasHeight;
+
 
 	resizeCanvas();
 
@@ -40,13 +49,17 @@ window.addEventListener('load', () => {
 let resizeCanvas = () => {
 	scaleFactor = 1;
 
+	let isFullscreen = screen.height == window.innerHeight; // Could also use window.outerHeight
+	let heightOffset = isFullscreen ? 0 : allElementsHeight;
+
 	while (canvas.width * (scaleFactor + 1) / window.devicePixelRatio <= window.innerWidth
-		&& canvas.height * (scaleFactor + 1) / window.devicePixelRatio <= window.innerHeight) {
+		&& canvas.height * (scaleFactor + 1) / window.devicePixelRatio <= window.innerHeight - heightOffset) {
 		scaleFactor += 1;
 	}
 
 	if (canvas.width * scaleFactor / window.devicePixelRatio == window.innerWidth 
-		|| canvas.height * scaleFactor / window.devicePixelRatio == window.innerHeight) {
+		|| canvas.height * scaleFactor / window.devicePixelRatio == window.innerHeight 
+		|| isFullscreen) {
 		document.body.classList.add("fullScreen");
 	} else {
 		document.body.classList.remove("fullScreen");
