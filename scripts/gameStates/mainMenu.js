e493a2c64fcf2cbace50acb89009a6bee670f8f3
delphic <delphic.uk@gmail.com>
2022-02-19 09:59:52 +0000

    Added some colour to the title screen text

--- scripts/gameStates/mainMenu.js
+++ scripts/gameStates/mainMenu.js
@@ -1,10 +1,12 @@
 const Fury = require('../../fury/src/fury');
 const TextMesh = require('../textmesh');
 const GameState = require('./gameState');
-const { vec3 } = Fury.Maths;
+const { vec3, vec4 } = Fury.Maths;
 
 module.exports = (function(){
     let exports = {};
+
+    let rink = vec4.fromValues(1.0, 0.0, 0.4, 1.0);
 
     exports.create = (config) => {
         let { canvas, scene, uiScene, uiAtlas, changeState } = config;
@@ -15,13 +17,14 @@ module.exports = (function(){
 
         let title, lore, instructions, enterText;
 
-        let createText = (text, x, y) => {
+        let createText = (text, x, y, color) => {
             return TextMesh.create({ 
                 text: text,
                 scene: uiScene,
                 atlas: uiAtlas,
                 position: vec3.fromValues(x * uiAtlas.tileSize, y * uiAtlas.tileSize, 0),
-                alignment: TextMesh.Alignment.center
+                alignment: TextMesh.Alignment.center,
+                color: color
             });
         };
 
@@ -32,11 +35,11 @@ module.exports = (function(){
             // Add Color to title (Rink) and enter text
             // Add scale option to textMesh for Title -> x2
 
-            title = createText("Fury Roguelike", cx, h-8);
+            title = createText("Fury Roguelike", cx, h-8, rink);
             let y = Math.floor(h/2);
-            lore = createText("Seek the Amulet of Power to save your town from the monsters!", cx, y+4);
-            instructions = createText("Use the arrow keys to move and attack, and space to wait.", cx, y+2);
-            enterText = createText("1. Enter Dungeon", cx, y-8);
+            lore = createText("Seek the Amulet of Power to save your town from the monsters!", cx, y+5);
+            instructions = createText("Use the arrow keys to move and attack, and space to wait.", cx, y+1);
+            enterText = createText("1. Enter Dungeon", cx, y-8, rink);
 
             scene.render();
             uiScene.render();
